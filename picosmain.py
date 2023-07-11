fe78737eb8c766523c7755e5400a40661af29c01
Jean-Frederic Clere <jfclere@gmail.com>
2023-07-11 19:42:12 +0200

    Add leds for the fun.

--- picosmain.py
+++ picosmain.py
@@ -11,18 +11,29 @@ conf = wifi.Picow()
 econnect = False
 deepsleep = True
 sleeptime = 60
+red = 22
+green = 21
+blue = 20
+
+pin_red = Pin(red, Pin.OUT, 0)
+pin_green = Pin(green, Pin.OUT, 0)
+pin_blue = Pin(blue, Pin.OUT, 0)
 
 pin_usb = Pin('WL_GPIO2', Pin.IN)
 usb = pin_usb.value()
 if usb == 1:
     print("USB connected")
+    pin_green.on()
     deepsleep = False
 
 while True:
     # the string we want to write
+    if usb == 1:
+        pin_green.on()
     try:
         conf.connectwifi()
         econnect = False
+        pin_blue.on()
         if not deepsleep:
             print("after conf.connectwifi(()")
     except Exception as e:
@@ -37,6 +48,7 @@ while True:
         mess = bytes(mess, 'utf-8')
         try:
             conf.sendserver(mess)
+            pin_red.on()
             if not deepsleep:
                 print("after conf.sendserver()!")
         except:
@@ -53,6 +65,9 @@ while True:
         if not deepsleep:
             print("exception in conf.disconnectwifi()!")
 
+    pin_red.off()
+    pin_blue.off()
+    pin_green.off()
     if not deepsleep:
         print("Sleeping: " + str(sleeptime))
         time.sleep(sleeptime)
