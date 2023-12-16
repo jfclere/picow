794dc08fb2aaf30f3eaa76060ae9d14e299ef4de
Jean-Frederic Clere <jfclere@gmail.com>
2023-12-16 12:43:17 +0100

    Add a string to install software in picow

---
+++ ocean.sh
@@ -0,0 +1,13 @@
+ampy --port /dev/ttyACM0 put wifi.py
+ampy --port /dev/ttyACM0 put picow.conf
+ampy --port /dev/ttyACM0 put ocean.py main.py
+ampy --port /dev/ttyACM0 put writereg.py
+ampy --port /dev/ttyACM0 put readreg.py
+ampy --port /dev/ttyACM0 put nodeinfo.py
+ampy --port /dev/ttyACM0 put reportserver.py
+ampy --port /dev/ttyACM0 put wpa_supplicant.py
+ampy --port /dev/ttyACM0 put wpa_supplicant.conf
+ampy --port /dev/ttyACM0 put lets-encrypt-r3.der
+ampy --port /dev/ttyACM0 put base64.py
+ampy --port /dev/ttyACM0 put $HOME/.netrc
+ampy --port /dev/ttyACM0 put $HOME/machine-id
