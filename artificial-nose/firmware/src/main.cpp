3c7e4cfa1d0cfde3cc01f8aaa61e8f2bf0ec0073
Jean-Frederic Clere <jfclere@gmail.com>
2024-03-06 19:23:44 +0100

    Add wifi support... Need to figure out TLS-1.3...

--- artificial-nose/firmware/src/main.cpp
+++ artificial-nose/firmware/src/main.cpp
@@ -9,6 +9,10 @@
 #include <Wire.h>
 #include <queue>
 
+#include <WiFi.h>
+
+#include "arduino_secrets.h"
+
 /**
 * @brief      Printf function uses vsnprintf and output using Arduino Serial
 *
@@ -84,17 +88,30 @@ static int fan_state = INITIAL_FAN_STATE;
 
 static bool debug_nn = false; // Set this to true to see e.g. features generated
                               // from the raw signal
+const char ssid[] = MY_SSID;
+const char pass[] = MY_PASS;
+
+void connecttowifi();
+bool sendtojfc();
 
 void setup()
 {
 
   Serial.begin(115200);
+  while (!Serial) {
+     // wait for serial port to connect. Needed for native USB port only
+  }
 
   pinMode(D0, OUTPUT);
   digitalWrite(D0, fan_state);
 
   pinMode(D0, OUTPUT);
   digitalWrite(D0, INITIAL_FAN_STATE);
+
+  connecttowifi();
+  IPAddress ip  = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
 
   gas->begin(Wire, 0x08); // use the hardware I2C
  
@@ -194,6 +211,12 @@ void loop()
         ei_printf(lineBuffer);
       }
 
+      /* send something to the server */
+      if (!sendtojfc()) {
+        Serial.print("Something is fishy...");
+        // WiFi.end(); not in arduino-esp32 ...
+        connecttowifi();
+      }
 /*
       if (screen_mode == INFERENCE_RESULTS) {
         if (best_prediction != latest_inference_idx) {
@@ -252,4 +275,91 @@ void loop()
     }
 
     **/
+}
+
+/* For the other part of example  */
+char SERVER[] = "jfclere.myddns.me";
+char BASE_URI[] = "/webdav/temp.txt";
+int READ_TIMEOUT = 60;
+
+
+void connecttowifi() {
+  // attempt to connect to Wifi network:
+  int status = WL_IDLE_STATUS;
+  WiFi.mode(WIFI_STA);
+  while (status != WL_CONNECTED) {
+
+    Serial.print("Attempting to connect to WPA SSID: ");
+
+    Serial.println(ssid);
+
+    // Connect to WPA/WPA2 network:
+
+    WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+
+    delay(10000);
+    WiFi.waitForConnectResult();
+    status = WiFi.status();
+
+  }
+}
+
+bool sendtojfc() {
+  WiFiClient client;
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (!client.connect(SERVER, 443)) {
+    Serial.println("Connection failed!");
+    client.stop();
+    return false;
+  } else {
+    Serial.println("Connected ...");
+    Serial.print("GET ");
+    Serial.print(BASE_URI);
+    Serial.println(" HTTP/1.1");
+    Serial.print("Host: ");
+    Serial.println(SERVER);
+    Serial.println("User-Agent: LED Map Client");
+    Serial.println("Connection: close");
+    Serial.println();
+    // Make a HTTP request, and print it to console:
+    client.print("GET ");
+    client.print(BASE_URI);
+    client.println(" HTTP/1.1");
+    client.print("Host: ");
+    client.println(SERVER);
+    client.println("User-Agent: LED Sectional Client");
+    client.println("Connection: close");
+    client.println();
+    client.flush();
+    unsigned long t = millis(); // start time
+
+    Serial.print("Getting data");
+
+    while (!client.connected()) {
+      if ((millis() - t) >= (READ_TIMEOUT * 1000)) {
+        Serial.println("---Timeout---");
+        client.stop();
+        return false;
+      }
+      Serial.print(".");
+      delay(1000);
+    }
+
+    // Read the response
+    while (client.connected()) {
+
+       if (client.available()) {
+
+          char c = client.read();
+
+          Serial.write(c);
+       }
+  
+    }
+    client.stop();
+  }
+  return true;
 }
