1cc2adbf5206db810626520237757a48b1de8db1
Jean-Frederic Clere <jfclere@gmail.com>
2025-06-07 12:01:06 +0200

    Add logic to stop charging.

--- gardenmain.py
+++ gardenmain.py
@@ -27,8 +27,11 @@ import time
 import machine
 import sys
 from machine import Pin
-import myadc
 import os
+
+# import our stuff
+import myadc
+from myprint import cantprint, canprint, myprint
 
 bat_adc = myadc.myadc(1)
 hyd_adc = myadc.myadc(0)
@@ -47,30 +50,62 @@ pin_hyd = Pin(HYDLED, Pin.OUT, 0)
 pin_pan.off()
 pin_hyd.on()
 
+# works only on picow (WL_ is wifi!)
+try:
+  pin_usb = Pin('WL_GPIO2', Pin.IN)
+  usb = pin_usb.value()
+  if usb == 1:
+    print("USB connected")
+    canprint()
+  else:
+    cantprint()
+except:
+  print("Something was wrong...")
+  try:
+    SIE_STATUS=const(0x50110000+0x50)
+    CONNECTED=const(1<<16)
+    SUSPENDED=const(1<<4)
+    if (machine.mem32[SIE_STATUS] & (CONNECTED | SUSPENDED))==CONNECTED:
+      print("USB connected")
+      canprint()
+    else:
+      cantprint()
+  except:
+    print("Something more was wrong...")
+
 # next time to change the panel connection
 deadline = time.ticks_add(time.ticks_ms(), DELAYCHANGE)
+charging = True
 while True:
   pin_hyd.toggle()
-  val = bat_adc.readval()
+  led.toggle()
+  valb = bat_adc.readval()
   # 4.7k + 47k (should be ~ 11)
-  val = val * BATFACTOR # my resistors are crappy!!!
-  mess = "Bat Val  : " + str(round(val, 2)) + "V "
-  # after the deadline: if more that BATHIGH disconnect the pannel
-  if time.ticks_diff(deadline, time.ticks_ms()) < 0:
-    # refresh deadline
-    deadline = time.ticks_add(time.ticks_ms(), DELAYCHANGE)
-    if val > BATHIGH :
-      pin_pan.on()
-    else:
-      pin_pan.off()
+  valb = valb * BATFACTOR # my resistors are crappy!!!
+  mess = "Bat Val  : " + str(round(valb, 2)) + "V "
    
   val = hyd_adc.readval()
   mess = mess + "Hyd Val  : " + str(round(val, 2)) + " "
   val = sol_adc.readval()
+
   # 4.7k + 47k (should be ~ 11)
   val = val * SOLFACTOR # my resistors are crappy!!!
   mess = mess + "Sol Val  : " + str(round(val, 2)) + "V"
-  print(mess)
+
+  # after the deadline: if more that BATHIGH disconnect the pannel
+  if time.ticks_diff(deadline, time.ticks_ms()) < 0:
+    # refresh deadline
+    deadline = time.ticks_add(time.ticks_ms(), DELAYCHANGE)
+    if valb > BATHIGH :
+      pin_pan.on()
+      if charging:
+        myprint(mess)
+      charging = False
+    else:
+      pin_pan.off()
+      if not charging:
+        myprint(mess)
+      charging = True
   time.sleep(1)
 
 pin_pan.off()
